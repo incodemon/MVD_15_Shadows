aab45a32f4696637830cb89191f21bf9d7eb32f7
Alun <alun@alunthomasevans.co.uk>
2020-02-23 18:41:15 +0100

    Added initDepth code

--- src/GraphicsUtilities.cpp
+++ src/GraphicsUtilities.cpp
@@ -141,7 +141,40 @@ void Framebuffer::initColor(GLsizei w, GLsizei h) {
 }
 
 void Framebuffer::initDepth(GLsizei w, GLsizei h) {
-	width = w; height = h;
+    width = w; height = h;
 
+    //bind framebuffer and texture as usual
+    glGenFramebuffers(1, &framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
 
+    //bind texture, but format with be only storing depth component
+    glGenTextures(1, &(color_textures[0]));
+    glBindTexture(GL_TEXTURE_2D, color_textures[0]);
+    //generate depth texture
+    glTexImage2D(GL_TEXTURE_2D, 0,
+        GL_DEPTH_COMPONENT, width, height, 0,
+        GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    //wrap filters to make sure shadow stops at edge of frustum
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+    //set border color so no risk of shadowing
+    float borderColor[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+
+    //attach this texture to the framebuffers depth attachment
+    //so anything drawn to this framebuffer is essentially just stored as depth, no colour
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, color_textures[0], 0);
+    //tell openGL we are not going to draw to a color buffer - if we don't say this then the
+    //framebuffer is incomplete and we get an error
+    glDrawBuffer(GL_NONE);
+    glReadBuffer(GL_NONE);
+
+    //safety first, safety second
+
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+        std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!" << std::endl;
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 }
